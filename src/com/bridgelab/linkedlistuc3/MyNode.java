97ffe95a1dac5fbffa2cea8caed9f9657f53b039
Ameet Jadhav <ammugs11@gmail.com>
2022-09-12 19:43:00 +0530

    added appended element last linked list

---
+++ src/com/bridgelab/linkedlistuc3/MyNode.java
@@ -0,0 +1,49 @@
+package com.bridgelab.linkedlistuc3;
+
+public class MyNode<K> {
+	public INode head;
+	public INode tail;
+	
+	public MyNode() {
+	    this.head = null;
+	    this.tail = null;
+	}
+
+	public void add(INode newNode) {
+		if(this.tail == null)
+		this.tail = newNode;
+	       if (this.head == null)
+		   this.head = newNode;
+	       else {
+		   INode temp = this.head; 
+		   this.head= newNode;
+		  head.setNext(temp);
+		}	
+	}
+	
+	public void append(INode myNode) {
+		if (this.tail == null)
+			this.tail = myNode;
+		if (this.head == null) {
+			this.head = myNode;
+		}
+		else {
+			INode temp = this.head;
+			this.tail.setNext(myNode);
+			this.tail = myNode;
+		}
+	}
+	
+	public void  printNodes() {
+		StringBuffer myNodes = new StringBuffer("My Nodes :");
+		INode temp = head;
+		while(temp.getNext() != null) {
+			myNodes.append(temp.getKey());
+			if(!temp.equals(tail))myNodes.append("->");
+				temp = temp.getNext();	
+		}
+		myNodes.append(temp.getKey());
+		System.out.println(myNodes);
+	} 
+
+}
