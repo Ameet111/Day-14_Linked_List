1b4c3c36aa063602e6f8f5a90312011910405bae
Ameet Jadhav <ammugs11@gmail.com>
2022-09-12 20:05:27 +0530

    added delete to first element using pop

---
+++ src/com/bridgelab/linkedlistuc5/MyNode.java
@@ -0,0 +1,62 @@
+package com.bridgelab.linkedlistuc5;
+
+public class MyNode<K> {
+	public INode head;
+	public INode tail;
+
+	public MyNode() {
+		this.head = null;
+		this.tail = null;
+	}
+
+	public void add(INode newNode) {
+		if (this.tail == null)
+			this.tail = newNode;
+		if (this.head == null)
+			this.head = newNode;
+		else {
+			INode temp = this.head;
+			this.head = newNode;
+			head.setNext(temp);
+		}
+	}
+
+	public void append(INode myNode) {
+		if (this.tail == null)
+			this.tail = myNode;
+		if (this.head == null) {
+			this.head = myNode;
+		} else {
+			INode temp = this.head;
+			this.tail.setNext(myNode);
+			this.tail = myNode;
+		}
+	}
+
+	public void insert(INode myNode, INode myNewNode) {
+		INode temp = myNode.getNext();
+		myNode.setNext(myNewNode);
+		myNewNode.setNext(temp);
+	}
+	
+	public INode pop(){
+		INode temp = this.head;
+		this.head = head.getNext();
+		return  temp;
+	}
+	
+	public void printNodes() {
+		StringBuffer myNodes = new StringBuffer("My Nodes :");
+		INode temp = head;
+		while (temp.getNext() != null) {
+			myNodes.append(temp.getKey());
+			if (!temp.equals(tail))
+				myNodes.append("->");
+			temp = temp.getNext();
+		}
+		myNodes.append(temp.getKey());
+		System.out.println(myNodes);
+	}
+ 
+
+}
---
+++ src/com/bridgelab/linkedlistuc5/MyNodeTest.java
@@ -0,0 +1,30 @@
+package com.bridgelab.linkedlistuc5;
+
+public class MyNodeTest {
+	
+	private static final boolean isExist = false;
+
+	public static void main(String[] args) {
+		given3NumbersWhenInsertingSecondInBetweenShouldPassLinkedListResult();
+	}
+
+	public static void given3NumbersWhenInsertingSecondInBetweenShouldPassLinkedListResult() {
+		Node<Integer> firstNumberNode = new Node<Integer>(56);
+		Node<Integer> secondNumberNode = new Node<Integer>(30);
+		Node<Integer> thirdNumberNode = new Node<Integer>(70);
+		MyNode myNode = new MyNode();
+		myNode.add(firstNumberNode);
+		myNode.append(thirdNumberNode);
+		myNode.insert(firstNumberNode, secondNumberNode);
+		myNode.printNodes();
+		boolean result = myNode.head.equals(firstNumberNode) && myNode.head.getNext().equals(secondNumberNode)
+				&& myNode.tail.equals(thirdNumberNode);
+		myNode.pop();
+		System.out.println("Linked List after deleting first element");
+		myNode.printNodes();
+		System.out.println(result);
+		
+		
+	}
+
+}
