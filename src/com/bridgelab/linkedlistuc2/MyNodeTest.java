cf0a5a34eb43402de88616218eee0245dbf5fc3a
Ameet Jadhav <ammugs11@gmail.com>
2022-09-12 19:34:27 +0530

    added element linked list

---
+++ src/com/bridgelab/linkedlistuc2/MyNodeTest.java
@@ -0,0 +1,21 @@
+package com.bridgelab.linkedlistuc2;
+
+public class MyNodeTest {
+	
+	public static void main(String[] args) {
+
+		Node<Integer> firstNumberNode = new Node<Integer>(70);
+		Node<Integer> secondNumberNode = new Node<Integer>(30);
+		Node<Integer> thirdNumberNode = new Node<Integer>(56);
+		MyNode myNode = new MyNode();
+		myNode.add(firstNumberNode);
+		myNode.add(secondNumberNode);
+		myNode.add(thirdNumberNode);
+		myNode.printNodes();
+
+		boolean result = myNode.head.equals(thirdNumberNode) && myNode.head.getNext().equals(secondNumberNode)
+				&& myNode.tail.equals(firstNumberNode);
+		System.out.println(result);
+	}
+
+}
